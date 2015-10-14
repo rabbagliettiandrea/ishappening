a9e471ba5ad2f55cb9d791247ad1ca7524a32165
Andrea <silverfix@gmail.com>
2015-10-14 17:40:57 +0200

    refactor

--- settings/__init__.py
+++ settings/__init__.py
@@ -5,17 +5,9 @@ from krakenv.tentacle import Tentacle
 
 
 TENTACLES = {
-    'andre.krakenv.io': Tentacle(
-        path='wraith.tentacles.default', database='default'),
-    'andre.pourich.com': Tentacle(
-        path='wraith.tentacles.pourich', database='pourich'),
-    'andre.spesafacile.it': Tentacle(
-        path='wraith.tentacles.spesafacile', database='spesafacile'),
+    'andre.ishappening.today': Tentacle(
+        path='wraith.tentacles.ishappening', database='ishappening'),
 }
 
 
-APPS = [
-    'wraith.apps.googleads_manager',
-    'wraith.apps.magento_manager',
-    'wraith.apps.web_crawling',
-]
+APPS = []
--- settings/dev_feather.py
+++ settings/dev_feather.py
@@ -6,5 +6,3 @@ from krakenv.settings import *
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 RSA_FILEPATH = r'~\.ssh\id_rsa'
-REDIS_SERVERS['magento_pourich'] = 'redis://192.168.30.233:6379/0'
-REDIS_SERVERS['magento_spesafacile'] = 'redis://192.168.30.233:6379/1'
