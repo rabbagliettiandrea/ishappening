092c7dacc6f77b311eea2e25e74ae509bd441b12
Andrea <silverfix@gmail.com>
2015-10-22 22:09:05 +0000

    deploy ishappening/prod

--- ishappening/settings/prod.py
+++ ishappening/settings/prod.py
@@ -6,9 +6,9 @@ from ishappening.settings._common import *
 
 DEBUG = False
 DATABASES['default']['CONN_MAX_AGE'] = 600
-# for TEMPLATE in TEMPLATES:
-#     TEMPLATE['options']['loaders'] = \
-#         ('django.template.loaders.cached.Loader', [
-#             'django.template.loaders.filesystem.Loader',
-#             'django.template.loaders.app_directories.Loader'
-#         ])
+for TEMPLATE in TEMPLATES:
+    TEMPLATE['OPTIONS']['loaders'] = \
+        ('django.template.loaders.cached.Loader', [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader'
+        ])
