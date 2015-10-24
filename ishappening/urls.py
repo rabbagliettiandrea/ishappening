5a9f3f1ad084f3b41f757a3961738ebe08f8f370
Andrea <silverfix@gmail.com>
2015-10-24 12:47:59 +0000

    deploy ishappening/prod

--- ishappening/urls.py
+++ ishappening/urls.py
@@ -2,6 +2,7 @@
 
 from __future__ import unicode_literals, division, absolute_import
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
 from ishappening import views, admin
 from ishappening.sitemap import sitemap_dict
 
@@ -14,5 +15,7 @@ urlpatterns = [
     url(r'^(?P<country_slug>[\w-]+)/(?P<internal_url>.+)/$', views.serve_document, name='serve_document'),
     # Sitemap
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.index', {'sitemaps': sitemap_dict}),
-    url(r'^sitemap-(?P<section>.+)\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemap_dict})
+    url(r'^sitemap-(?P<section>.+)\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemap_dict}),
+    # robots.txt
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'))
 ]
