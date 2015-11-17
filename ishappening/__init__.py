a582364a7c83ca4e07de56df901b35cec8d5e225
silverfix <rabbagliettiandrea@gmail.com>
2015-11-17 16:19:57 +0100

    fixed custom site admin

--- ishappening/__init__.py
+++ ishappening/__init__.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals, division, absolute_import
+from django.contrib import admin
+from ishappening.admin import site as custom_site
+
+
+def _handle_admin():
+    """
+    Admin site-wide setup
+    """
+    admin.site = custom_site
+
+
+_handle_admin()
--- ishappening/admin.py
+++ ishappening/admin.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals, division, absolute_import
-from django.contrib.admin import ModelAdmin
+from django.contrib.admin import ModelAdmin, AdminSite
 from django_zilla.utils.admin_mixin import LinkOnSiteMixin
 from ishappening.models import Document
-from django_zilla.utils.admin import AdminSite
 
 
 site = AdminSite()
@@ -16,5 +15,4 @@ class DocumentAdmin(LinkOnSiteMixin, ModelAdmin):
     list_display = ['title', 'link_on_site']
 
 
-site.register_from_default_site()
 site.register(Document, DocumentAdmin)
